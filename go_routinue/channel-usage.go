1c2b80f9a324459f213e2d4128132741d9e2602c
vans8800 <muwang86@126.com>
2025-02-06 20:42:26 +0800

    采用channel实现等待组的功能

--- go_routinue/channel-usage.go
+++ go_routinue/channel-usage.go
@@ -5,34 +5,67 @@ import (
 	"time"
 )
 
-func main() {
-	//创建一个容量为1的channel
-	ch := make(chan int, 1)
+// 使用channel实现两个goroutine的交替执行, 实现协程同步--互斥
+// func main() {
+// 	//创建一个容量为1的channel
+// 	ch := make(chan int, 1)
 
-	//向channel中写入数据: 0
-	ch <- 0
+// 	//向channel中写入数据: 0
+// 	ch <- 0
+
+// 	go func() {
+// 		//从channel中读取数据
+// 		<-ch
+// 		for i := 1; i <= 5; i++ {
+// 			time.Sleep(time.Millisecond)
+// 			fmt.Println(i)
+// 		}
+
+// 		//执行结束,向channel中写入数据: 0
+// 		ch <- 0
+// 	}()
+
+// 	go func() {
+// 		//从channel中读取数据
+// 		<-ch
+// 		for i := 1; i <= 5; i++ {
+// 			time.Sleep(time.Millisecond)
+// 			fmt.Println(i * 10)
+// 		}
+// 		//执行结束,向channel中写入数据: 0
+// 		ch <- 0
+// 	}()
+// 	time.Sleep(time.Second)
+// }
+
+//采用channel实现等待组的功能
+func main() {
+	count := 2
+	//创建一个无缓冲的chanel
+	ch := make(chan int)
 
 	go func() {
-		//从channel中读取数据
-		<-ch
 		for i := 1; i <= 5; i++ {
-			time.Sleep(time.Millisecond)
+			time.Sleep(time.Millisecond) //模拟耗时操作
 			fmt.Println(i)
 		}
-
-		//执行结束,向channel中写入数据: 0
+		//子协程执行结束,向channel中写入数据: 0
 		ch <- 0
 	}()
 
 	go func() {
-		//从channel中读取数据
-		<-ch
 		for i := 1; i <= 5; i++ {
-			time.Sleep(time.Millisecond)
+			time.Sleep(time.Millisecond) //模拟耗时操作
 			fmt.Println(i * 10)
 		}
-		//执行结束,向channel中写入数据: 0
+		//子协程执行结束,向channel中写入数据: 0
 		ch <- 0
 	}()
-	time.Sleep(time.Second)
+
+	for i := 0; i < count; i++ {
+		//主协程中,从channel中读取数据
+		<-ch
+	}
+
+	fmt.Println("main goroutine exit")
 }
